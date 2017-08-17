c4c4fd246b759e27e1516e552859afce04460eea
Shawn M Moore <shawn@bestpractical.com>
2017-08-17 18:42:34 +0000

    Add a (hidden) state output

--- static/js/lifecycleui-editor.js
+++ static/js/lifecycleui-editor.js
@@ -3,6 +3,9 @@ jQuery(function () {
         var container = jQuery(this);
         var name = container.data('name');
         var config = RT.LifecycleConfig[name];
+        var stateOutput = container.find('.state');
+        stateOutput.text(JSON.stringify(config));
+
     });
 });
 
