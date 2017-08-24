50064eb3e23647dc810a336bc0164820ece368f4
Shawn M Moore <shawn@bestpractical.com>
2017-08-24 18:05:41 +0000

    Functions to round trip between config and editor state

--- static/js/lifecycleui-editor.js
+++ static/js/lifecycleui-editor.js
@@ -15,6 +15,70 @@ jQuery(function () {
               .attr('fill', 'black');
     };
 
+    var newState = function (clone) {
+        return {
+            statuses: [],
+            defaults: {},
+            transitions: {},
+            rights: {},
+            actions: [],
+
+            statusMeta: {}
+        };
+    };
+
+    var initializeStateFromConfig = function (config) {
+        var state = newState();
+
+        jQuery.each(['initial', 'active', 'inactive'], function (i, type) {
+            if (config[type]) {
+                state.statuses = state.statuses.concat(config[type]);
+                jQuery.each(config[type], function (j, statusName) {
+                    state.statusMeta[statusName] = {
+                        type: type
+                    };
+                });
+            }
+        });
+
+        if (config.defaults) {
+            state.defaults = config.defaults;
+        }
+
+        if (config.transitions) {
+            state.transitions = config.transitions;
+        }
+
+        if (config.rights) {
+            state.rights = config.rights;
+        }
+
+        if (config.actions) {
+            state.actions = config.actions;
+        }
+
+        return state;
+    };
+
+    var exportConfigFromState = function (state) {
+        var config = {
+            initial: [],
+            active: [],
+            inactive: [],
+            defaults: state.defaults,
+            actions: state.actions,
+            rights: state.rights,
+            transitions: state.transitions
+        };
+
+        jQuery.each(state.statuses, function (i, statusName) {
+            var statusType = state.statusMeta[statusName].type;
+            config[statusType].push(statusName);
+        });
+
+        return config;
+    };
+
     var initializeEditor = function (node) {
         var container = jQuery(node);
         var name = container.data('name');
@@ -23,7 +87,7 @@ jQuery(function () {
         var svg = d3.select(node)
                     .select('svg');
 
-        console.log(config);
+        var state = initializeStateFromConfig(config);
 
         createArrowHead(svg);
     };
