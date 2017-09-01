30d9a3883a8b860fc374db778f2e4ea6d50e4825
Shawn M Moore <shawn@bestpractical.com>
2017-09-01 14:32:28 +0000

    Rename css file for parity

--- lib/RT/Extension/LifecycleUI.pm
+++ lib/RT/Extension/LifecycleUI.pm
@@ -12,7 +12,7 @@ RT->AddJavaScript("lifecycleui-viewer.js");
 RT->AddJavaScript("lifecycleui-editor.js");
 
 RT->AddStyleSheets("lifecycleui.css");
-RT->AddStyleSheets("lifecycleui-display.css");
+RT->AddStyleSheets("lifecycleui-viewer.css");
 RT->AddStyleSheets("lifecycleui-editor.css");
 
 $RT::Config::META{Lifecycles}{EditLink} = RT->Config->Get('WebURL') . 'Admin/Lifecycles/';
