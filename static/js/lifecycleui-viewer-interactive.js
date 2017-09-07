67e19b975e7ff00da99e4962a48495ce84a01c9d
Shawn M Moore <shawn@bestpractical.com>
2017-09-07 20:50:29 +0000

    Open menu near the currently-selected status

--- static/js/lifecycleui-viewer-interactive.js
+++ static/js/lifecycleui-viewer-interactive.js
@@ -6,11 +6,34 @@ jQuery(function () {
     };
     Interactive.prototype = Object.create(Super.prototype);
 
+    Interactive.prototype._setMenuPosition = function () {
+        if (!this.selectedStatus) {
+            return;
+        }
+
+        var d = this.selectedStatus;
+        var circle = this.statusContainer.select('circle[data-key="'+ d._key + '"]');
+        var bbox = circle.node().getBoundingClientRect();
+        var x = bbox.right + window.scrollX;
+        var y = bbox.top + window.scrollY;
+
+        this.selectedMenu.css({top: y, left: x});
+    };
+
     Interactive.prototype.clickedStatus = function (d) {
+        var statusName = d.name;
+        this.selectedMenu = this.menuContainer.find('.status-menu[data-status="'+statusName+'"]');
+        this.selectedStatus = d;
+
+        this.menuContainer.find('.status-menu.selected').removeClass('selected');
+        this.selectedMenu.addClass('selected');
+
+        this._setMenuPosition();
     };
 
     Interactive.prototype.initializeViewer = function (node, name, config, focusStatus) {
          Super.prototype.initializeViewer.call(this, node, name, config, focusStatus);
+         this.menuContainer = jQuery(node).find('.status-menus');
     };
 
     RT.LifecycleViewerInteractive = Interactive;
